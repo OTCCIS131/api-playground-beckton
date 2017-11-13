905c9c6f727338df0739a60d97c3ac2da954d584
Bec <bec.braughton@gmail.com>
2017-11-12 20:21:26 -0600

    ...
    I can't even get the example to work

---
+++ main.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    const vm = new Vue({
+        el: '#app',
+        data: {
+            loading: false,
+            showing: 'films',
+            films: [],
+            film: null,
+            people: []
+        },
+        methods: {
+            loadFilms() {
+                this.loading = true
+                this.$http.get('https://ghibliapi.herokuapp.com')
+                    .then(resp => {
+                        this.films = resp.body
+                        this.loading = false
+                    })
+            },
+            selectFilm(film) {
+                this.film = film
+                this.showing = 'people'
+                this.loadPeople()
+            },
+            loadPeople() {
+                //this.loading = true
+                this.people = []                
+                this.film.people.forEach(person => {
+                    this.$http.get(person)
+                        .then(resp => {
+                            this.people.push(resp.body)
+                        })
+                })
+            }    
+        }
+    })
+})

b0d6f30e93d226eddda7e9bfbf4bab6ab0db0130
Bec <bec.braughton@gmail.com>
2017-11-29 14:58:31 -0600

    Nearly working Dog API call
    Needs images to show after loading

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
 $(function() {
-
+    Vue.filter('capitalize', function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      })
     const vm = new Vue({
         el: '#app',
         data: {
             loading: false,
-            showing: 'films',
-            films: [],
-            film: null,
-            people: []
+            showing: 'breeds',
+            breeds: [],
+            images: []
         },
         methods: {
-            loadFilms() { 
+            loadBreeds() {
                 this.loading = true
-                this.$http.get('https://ghibliapi.herokuapp.com/films')
+                this.$http.get('https://dog.ceo/api/breeds/list')
                     .then(resp => {
-                        this.films = resp.body
+                        this.breeds = resp.body.message
                         this.loading = false
                     })
             },
-            selectFilm(film) {
-                this.film = film
-                this.showing = 'people'
-                this.loadPeople()
-            },
-            loadPeople() {
-                //this.loading = true
-                this.people = []                
-                this.film.people.forEach(person => {
-                    this.$http.get(person)
-                        .then(resp => {
-                            this.people.push(resp.body)
-                        })
+            loadBreedImages(breed) {
+                this.$http.get('https://dog.ceo/api/breed/' + breed + '/images')
+                .then(resp => {                    
+                    this.images = resp.body.message
+                    this.showing = 'images'
                 })
-            }    
+            }
         }
     })
 })
